ee5fc650d1d1e7a670c8c8d7f4742009429dce54
pry <pranoydll@gmail.com>
2020-05-13 13:36:41 -0500

    added basic flask app

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/login')
+def login():
+    return 'TODO!'
+
+@app.route('/fetch', methods=['POST'])
+def fetch():
+    return 'TODO!'
+
+@app.route('/taken', methods=['POST'])
+def taken():
+    return 'TODO!'
+
+
