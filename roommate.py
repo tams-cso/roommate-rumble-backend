78a6c236e938a513ed4644e3cdcabf37ced2bace
Daniel Ting <danielzting@gmail.com>
2020-05-21 15:15:00 -0500

    Write comparison algorithm

--- roommate.py
+++ roommate.py
@@ -1,9 +1,13 @@
 """
 roommate.py - roommate matching library
 
-Logic itself is very "hackathon-y" but the overall data, algorithm, and
-presentation are self-contained and decoupled so each individual section
-can be reused, refactored, and reincorporated into a real app if needed.
+The overall data, algorithm, and presentation are self-contained and
+decoupled so each individual section can be reused, refactored, and
+reincorporated into a real app later if needed.
+
+However, the logic itself is tightly coupled to the form schema, where
+one change there will necessitate edits in the weights, constructor,
+and normalization function. I am not sure if there is a way around this.
 """
 
 import math
@@ -12,8 +16,9 @@ class Student:
     """Store a student's responses."""
 
     # Weights for distance function
-    weights = {
-        "bedtime": 1,
+    WEIGHTS = {
+        "bedtime": 12, # So a one-hour difference in bedtime translates
+                       # to one distance unit
         "wake_time": 1,
         "sleep_env": 1,
         "visitors": 1,
@@ -24,10 +29,7 @@ class Student:
 
     def __init__(self, line):
         """
-        The single responsibility of this constructor is to construct a
-        Student from an array of plain strings. Therefore, this is the
-        only code needed to change in order to adapt to different data
-        representations.
+        Construct a Student from an array of plain strings.
 
         XXX: OK so this part isn't quite fully decoupled from the file
         reading. Probably shouldn't separate loading data between two
@@ -40,38 +42,78 @@ class Student:
         self.gender, self.bedtime, self.wake_time, self.sleep_env, \
         self.visitors, self.neatness, self.floor_cov, self.sharing, \
         self.bio, self.room_time, self.room_activity = line
+        self.traits = self.normalize()
 
     def distance(self, o):
         """
         Return the relative distance between the current and another
-        Student as a floating-point value. First, all trait differences
-        are normalized to a [0, 1] scale, except for bedtime, which
-        ranges over [0, 12] so that a one-hour difference would
-        translate to one distance unit. This should make it easier to
-        adjust the weights, because a default weight of 1 would have
-        approximately the same meaning across all attributes as distance
-        distance units are somewhat comparable between criteria.
+        Student as a floating-point value.
         """
         # No mixing grade levels or purpling
         if self.junior != o.junior or self.gender != o.gender:
             return math.inf
 
-class Matcher():
-    """Compute the distance matrix."""
-    def __init__(self):
-        self.students = []
+        sum = 0
+        for self_trait, o_trait in zip(self.traits, o.traits):
+            diff = abs(self.traits[self_trait] - o.traits[o_trait])
+            sum += Student.WEIGHTS[self_trait] * diff ** 2
+        return math.sqrt(sum)
 
-    def match(self):
+    def normalize(self):
         """
-        Return a distance matrix where the value at the i-th row and
-        the j-th column represents the relative distance between the two
-        roommates.
+        Return a dict of integer representations of traits, all
+        normalized to a [0, 1] scale. Where applicable, the scale goes
+        from most to least "strict."
         """
-        pass
+        traits = {}
 
-def Presenter():
-    """Export the results into a human-readable form."""
-    pass
+        # HACK: For bedtime, set 6 PM to 0 and 6 AM to 1. This makes
+        # time distance calculations easier but will break if someone
+        # enters a bedtime outside of this range.
+        hour = int(self.bedtime.split(":")[0]) + 6
+        minute = int(self.bedtime.split(":")[1])
+        traits["bedtime"] = hour % 12 / 12 + minute / 12 / 60
+
+        if self.wake_time == "Early, with time to spare":
+            traits["wake_time"] = 0
+        elif self.wake_time == "Just in time to get to class":
+            traits["wake_time"] = 1/2
+        else:
+            traits["wake_time"] = 1
+
+        if self.sleep_env == "Unless it is absolutely quiet":
+            traits["sleep_env"] = 0
+        elif self.sleep_env == "With a TV or music on":
+            traits["sleep_env"] = 1/3
+        elif self.sleep_env == "If my roommate has visitors in the room":
+            traits["sleep_env"] = 2/3
+        else:
+            traits["sleep_env"] = 1
+
+        if self.visitors == "No one in my room. My room is only for myself and my roommate":
+            traits["visitors"] = 0
+        elif self.visitors == "Only a few people into my room.":
+            traits["visitors"] = 1/2
+        else:
+            traits["visitors"] = 1
+
+        if self.neatness == "Always neat and organized":
+            traits["neatness"] = 0
+        elif self.neatness == "Neat most of the time":
+            traits["neatness"] = 1/2
+        else:
+            traits["neatness"] = 1
+
+        traits["floor_cov"] = int(self.floor_cov) / 100
+
+        if self.sharing == "All my stuff is mine":
+            traits["sharing"] = 0
+        elif self.sharing == "Willing to share most stuff if I'm asked first":
+            traits["sharing"] = 1/2
+        else:
+            traits["sharing"] = 1
+
+        return traits
 
 if __name__ == "__main__":
     pass
