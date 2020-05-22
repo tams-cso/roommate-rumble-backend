22d5d035e4ea635edef533226e7411f43a61d46d
Daniel Ting <danielzting@gmail.com>
2020-05-21 20:22:34 -0500

    Write export logic

--- roommate.py
+++ roommate.py
@@ -10,6 +10,7 @@ one change there will necessitate edits in the weights, constructor,
 and normalization function. I am not sure if there is a way around this.
 """
 
+import csv
 import math
 
 class Student:
@@ -38,7 +39,7 @@ class Student:
         Builder pattern for this class, both of which are definitely
         worse than this?
         """
-        self.name, self.email, self.facebook, self.junior, \
+        self.time, self.name, self.email, self.facebook, self.junior, \
         self.gender, self.bedtime, self.wake_time, self.sleep_env, \
         self.visitors, self.neatness, self.floor_cov, self.sharing, \
         self.bio, self.room_time, self.room_activity = line
@@ -74,46 +75,78 @@ class Student:
         minute = int(self.bedtime.split(":")[1])
         traits["bedtime"] = hour % 12 / 12 + minute / 12 / 60
 
-        if self.wake_time == "Early, with time to spare":
+        if "Early" in self.wake_time:
             traits["wake_time"] = 0
-        elif self.wake_time == "Just in time to get to class":
+        elif "Just in time" in self.wake_time:
             traits["wake_time"] = 1/2
         else:
             traits["wake_time"] = 1
 
-        if self.sleep_env == "Unless it is absolutely quiet":
+        if "absolutely quiet" in self.sleep_env:
             traits["sleep_env"] = 0
-        elif self.sleep_env == "With a TV or music on":
+        elif "TV or music" in self.sleep_env:
             traits["sleep_env"] = 1/3
-        elif self.sleep_env == "If my roommate has visitors in the room":
+        elif "visitors" in self.sleep_env:
             traits["sleep_env"] = 2/3
         else:
             traits["sleep_env"] = 1
 
-        if self.visitors == "No one in my room. My room is only for myself and my roommate":
+        if "No one" in self.visitors:
             traits["visitors"] = 0
-        elif self.visitors == "Only a few people into my room.":
+        elif "few" in self.visitors:
             traits["visitors"] = 1/2
         else:
             traits["visitors"] = 1
 
-        if self.neatness == "Always neat and organized":
+        if "Always" in self.neatness:
             traits["neatness"] = 0
-        elif self.neatness == "Neat most of the time":
+        elif "most" in self.neatness:
             traits["neatness"] = 1/2
         else:
             traits["neatness"] = 1
 
         traits["floor_cov"] = int(self.floor_cov) / 100
 
-        if self.sharing == "All my stuff is mine":
+        if "stuff is mine" in self.sharing:
             traits["sharing"] = 0
-        elif self.sharing == "Willing to share most stuff if I'm asked first":
+        elif "Willing to share" in self.sharing:
             traits["sharing"] = 1/2
         else:
             traits["sharing"] = 1
 
         return traits
 
+    def __str__(self):
+        """Return the name of the Student."""
+        return self.name
+
 if __name__ == "__main__":
-    pass
+    """
+    This section actually runs the algorithm and exports the data
+    into a human-readable format.
+    """
+    # Read and store student data
+    students = []
+    with open("data.csv") as data:
+        data.readline() # Skip header
+        reader = csv.reader(data)
+        for row in reader:
+            students.append(Student(row))
+
+    # Generate dict of matches from closest to furthest
+    distances = {}
+    for student in students:
+        row = {}
+        for other in students:
+            row[other.name] = student.distance(other)
+        distances[student.name] = \
+            {k: v for k, v in sorted(row.items(), key=lambda item: item[1])}
+
+    # Write top 10 matches into another CSV
+    with open("results.csv", mode="w") as results:
+        writer = csv.writer(results)
+        for student in distances:
+            row = []
+            for other in distances[student]:
+                row.append(other + '(' + str(distances[student][other]) + ')')
+            writer.writerow(row)
